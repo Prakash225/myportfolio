5d3ebb2d95d27020948a917c04f2e15974d25b12
Prakash Prajapati <prakash56dec@gmail.com>
2024-02-15 19:56:49 +0530

    changes latest

--- js/general.js
+++ js/general.js
@@ -144,34 +144,3 @@ function opentab(obj) {
     $(obj).addClass('active');
 }
 
-
-// Image Slider
-$('#imgsliderbx').slick({
-    centerMode: true,
-    centerPadding: '15px',
-    dots: true,
-    infinite: true,
-    speed: 300,
-    adaptiveHeight: true,
-    slidesToShow: 3,
-    responsive: [
-        {
-            breakpoint: 768,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 3
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 1
-            }
-        }
-    ]
-});
